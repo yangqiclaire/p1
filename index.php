da169278d36bcb9b4a53b3ba1960ac1ee6285e91
Qi Yang <yangqi.qiqiy@gmail.com>
2014-09-19 01:21:20 -0400

    add index php

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html>
+    <head>
+       	<meta charset='utf-8'>
+       	<link type='text/css' rel="stylesheet" href="p1.css">
+       	<title>Qi Project 1</title>
+    </head>
+    <body>
+        <header>
+        	<h1>Qi Yang's Portfolio</h1>
+        </header>
+        <div id="table">
+	    <div id="tablerow">
+        <section id="projects">
+            <h2>My DWA Projects</h2>
+            <p>Project 1 : Portfolio</p>
+            <ul>
+               <li><a href="http://p1.claireyang.me/">View</a></li>
+               <li><a href="https://github.com/yangqiclaire/p1">Github</a></li>
+            </ul>
+            <p>Project 2 : Title TBD</p>
+            <ul>
+               <li><a href="http://p2.claireyang.me/">View</a></li>
+               <li><a href="https://github.com/yangqiclaire/p2">Github</a></li>
+            </ul>
+            <p>Project 3 : Title TBD</p>
+            <ul>
+               <li><a href="http://p3.claireyang.me/">View</a></li>
+               <li><a href="https://github.com/yangqiclaire/p3">Github</a></li>
+             </ul>
+            <p>Project 4 : Title TBD</p>
+            <ul>
+               <li><a href="http://p4.claireyang.me/">View</a></li>
+               <li><a href="https://github.com/yangqiclaire/p4">Github</a></li>
+            </ul>      	
+        </section>
+        <section id='main'>  
+              <a href="qiorigin.jpg"><img src="qi.jpg" alt="a picture of Qi Yang" id='qi'></a>
+              <h1>Qi Yang</h1>
+              <h2>Dynamic Web Application</h2>
+              <h3>Who am I and What do I do?</h3>
+              <p>I am a student at Harvard Extension School. I also work as a researcher at University of Pennsylvania. 
+              I am interested in software and web development. 
+              I plan to obtain a master degree in software engineer from Harvard in order to lauch a new career. 
+              </p>
+              <h3>Why am I taking this course?</h3>
+              <p>I am taking this course to learn the essentials of dynamic web development. </p>
+              <h3>What previous experience do I have?</h3>
+              <p>I have self-learned HTML and CSS. I also have experience with php and Javascript. </p>
+              <h3>Am I working on a Mac, PC, or some other platform? </h3>
+              <p>I am working on a PC. </p>
+              <h3>Anything else we should know?</h3>
+              <p>I wish to be able to know more about databases and the related resources for further learning. </p>
+        </section> 
+        </div>
+        </div>
+        
+       	
+    </body>
+</html>
